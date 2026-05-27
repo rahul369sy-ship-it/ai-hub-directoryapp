914caa6487e94e2cbf809b49abf45ca1920e70e9
rahul369sy-ship-it <rahul369sy@gmail.com>
2026-05-27 23:44:44 +0530

    Create main.jsx

---
+++ main.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import AIToolsDirectory from './AIToolsDirectory.jsx'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <AIToolsDirectory />
+  </React.StrictMode>,
+)
